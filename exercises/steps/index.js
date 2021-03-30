9a3dab535715f9e2f665e13a51ddf82d3992a843
Christopher Kang <christopherjkang24@gmail.com>
2021-03-30 17:30:50 -0400

    steps exercise complete with recurison

---
+++ exercises/steps/index.js
@@ -0,0 +1,55 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n, row = 0, stairs = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (stairs.length === n) {
+    console.log(stairs)
+    return steps(n, row + 1);
+  }
+
+  const add = stairs.length <= row ? '#' : ' ';
+  steps(n, row, stairs + add)
+}
+
+steps(3);
+
+module.exports = steps;
+
+// function steps(n) {
+  
+//   stepsCounter = '';
+
+//   for (let row = 0; row < n; row++) {
+//     let stairs = '';
+//     for (let col = 0; col < n; col++) {
+
+//       if (col <= row) {
+//         stairs += '#';
+//       } else {
+//         stairs += ' ';
+//       }
+//     }
+//     console.log(stairs);
+//   }
+  
+// }
